5dca1a65c8da4b0a365ea4baa375078751ca47b3
Nabaneeta10 <nabaneetamohanta10@gmail.com>
2024-05-11 12:47:05 +0530

    shell script

---
+++ 4th sem/DAA/OS/assignment3/qs1.sh
@@ -0,0 +1,12 @@
+echo "Enter your name:"
+read name
+
+echo "Enter the program name:"
+read program_name
+
+echo "Enter your registration number:"
+read registration_number
+
+echo "Name: $name"
+echo "Program Name: $program_name"
+echo "Registration Number: $registration_number"
