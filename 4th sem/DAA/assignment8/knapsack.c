d2a72197de11a078e87562a0f9e088bfc627a49d
Nabaneeta10 <nabaneetamohanta10@gmail.com>
2024-05-20 18:28:23 +0530

    knapsack

---
+++ 4th sem/DAA/assignment8/knapsack.c
@@ -0,0 +1,75 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void greedy_knapsack(int m, int n);
+int main()
+{
+    int m, n;
+    printf("Enter size of knapsack:");
+    scanf("%d", &m);
+    printf("Enter number of objects:");
+    scanf("%d", &n);
+    greedy_knapsack(m, n);
+    return 0;
+}
+
+void greedy_knapsack(int m, int n)
+{
+    int i;
+    float profits[n], weights[n], ratios[n], x[n];
+    printf("Enter profits and weights for each object:\n");
+    for(i=0;i<n;i++) {
+        scanf("%f %f", &profits[i], &weights[i]);
+        ratios[i]=profits[i]/weights[i];
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(ratios[j]<ratios[j+1])
+            {
+                float temp=ratios[j];
+                ratios[j]=ratios[j+1];
+                ratios[j+1]=temp;
+
+                temp=profits[j];
+                profits[j]=profits[j+1];
+                profits[j+1]=temp;
+
+                temp=weights[j];
+                weights[j]=weights[j+1];
+                weights[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        x[i]=0;
+    }
+    float u=m;
+    float totalProfit=0,totalWeight=0;
+    for(i=0;i<n;i++)
+    {
+        if(weights[i]>u) break;
+        x[i]=1;
+        totalProfit+=profits[i];
+        totalWeight+=weights[i];
+        u-=weights[i];
+    }
+    if(i<=n)
+    {
+        x[i]=u/weights[i];
+        totalProfit+=x[i]*profits[i];
+        totalWeight+=x[i]*weights[i];
+    }
+
+    printf("Selected fraction of each item:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%f ",x[i]);
+    }
+    printf("\n");
+    printf("Total profit is: %f\n", totalProfit);
+    printf("Total weight is: %f\n", totalWeight);
+}
